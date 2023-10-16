f52bf6d0141b6ce8efbba7ba46766c61ace6e02b
Somesh_vas <someshvas1@gmail.com>
2023-10-16 17:43:33 +0530

    CUDA revision

---
+++ Unique_index_calculationusingthreadidx_blockid_blockdim/kernel.cu
@@ -0,0 +1,76 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+//cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void uniq(int * input)
+{
+    int tid = threadIdx.x;
+    printf("threadIdx : %d, value : %d \n", tid, input[tid]);
+}
+__global__ void uniq_grid_1d(int* input) {
+    int tid = threadIdx.x;
+    int offset = blockIdx.x * blockDim.x;
+    int gid = tid + offset;
+    printf("blockIdx.x : %d, threadIdx.x : %d, grid : %d, value : %d \n",
+        blockIdx.x, tid, gid, input[gid]);
+}
+__global__ void uniq_grid_2d(int* input) {
+    int tid = threadIdx.x;
+    int block_offset = blockIdx.x * blockDim.x;
+    int row_offset = blockDim.x * gridDim.x * blockIdx.y;
+    int gid = tid + block_offset + row_offset;
+    printf("blockIdx.x : %d,blockIdx.y : %d, threadIdx.x : %d, grid : %d, value : %d \n",
+        blockIdx.x,blockIdx.y, tid, gid, input[gid]);
+}
+__global__ void uniq_grid_2d_2d(int* input) {
+    int tid = blockDim.x * threadIdx.y + threadIdx.x;
+    int num_threads_in_a_block = blockDim.x * blockDim.y;
+    int block_offset = blockIdx.x * num_threads_in_a_block;
+    int num_threads_in_a_row = num_threads_in_a_block * gridDim.x;
+    int row_offset = num_threads_in_a_row * blockIdx.y;
+    int gid = tid + block_offset + row_offset;
+    printf("blockIdx.x : %d,blockIdx.y : %d, threadIdx.x : %d, grid : %d, value : %d \n",
+        blockIdx.x, blockIdx.y, tid, gid, input[gid]);
+}
+
+
+/*
+* index =(blockIdx.X X blockDim.X) + threadIdx.X
+* index = offset + tid
+* 
+* general index calculation
+* index = row offset + block offset + tid
+* index = (number of threads in one thread block row * blockIdx.y) + number of threads in * thread block * blockIdx.x + threadIdx.x
+* 
+* -- number of threads in one row = gridDim.x * blockDim.x
+* -- number of threads in thread block = blockDim.x
+*/
+int main() {
+    int array_size = 16;
+    int array_byte_size = sizeof(int) * array_size;
+    int h_data[16];
+
+    for (int i = 0; i < array_size; i++) {
+        h_data[i] = i + 1;
+        printf("%d ", h_data[i]);
+    }
+
+    printf("\n \n");
+
+    int* d_data;
+    cudaMalloc((void**)&d_data, array_byte_size);
+    cudaMemcpy(d_data, h_data, array_byte_size, cudaMemcpyHostToDevice);
+
+    dim3 block(2,2);
+    dim3 grid(2,2);
+
+    uniq_grid_2d_2d<< < grid, block >> > (d_data);
+    cudaDeviceSynchronize();
+
+    cudaDeviceReset();
+    return 0;
+}
---
+++ assignment_1_3D_grid/kernel.cu
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+__global__ void printThreadInfo() {
+    int threadIdX = threadIdx.x;
+    int threadIdY = threadIdx.y;
+    int threadIdZ = threadIdx.z;
+
+    int blockIdX = blockIdx.x;
+    int blockIdY = blockIdx.y;
+    int blockIdZ = blockIdx.z;
+
+    int gridDimX = gridDim.x;
+    int gridDimY = gridDim.y;
+    int gridDimZ = gridDim.z;
+
+    printf("ThreadIdx: (%d, %d, %d)\n", threadIdX, threadIdY, threadIdZ);
+    printf("BlockIdx: (%d, %d, %d)\n", blockIdX, blockIdY, blockIdZ);
+    printf("GridDim: (%d, %d, %d)\n", gridDimX, gridDimY, gridDimZ);
+}
+
+int main() {
+    dim3 blockDimensions(2, 2, 2); // 2 threads in each dimension for the block
+    dim3 gridDimensions(2, 2, 2);   // 4 threads in all dimensions for the grid
+
+    printThreadInfo << <gridDimensions, blockDimensions >> > ();
+    cudaDeviceSynchronize(); // Wait for the GPU to finish
+
+    return 0;
+}
